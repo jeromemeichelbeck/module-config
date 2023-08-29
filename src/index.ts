29ded988fff44da500f661a7ab070b5c88dd6f19
Jérôme Meichelbeck <jerome.meichelbeck@focus.lu>
2023-08-29 12:11:57 +0200

    Install zod and basic createModuleConfig implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-console.log('Hello World!');
+import { z } from 'zod';
+
+type AccepptedType = string | number | boolean | undefined;
+
+type Field<T extends AccepptedType> = {
+  key: string;
+  schema: z.ZodType<T>;
+};
+
+export const createModuleConfig = <
+  const TField extends Field<TValue>,
+  TValue extends AccepptedType
+>(
+  fields: TField[]
+) => {
+  const getValue = <
+    K extends TField['key'],
+    U extends z.infer<Extract<(typeof fields)[number], { key: K }>['schema']>
+  >(
+    key: K
+  ) => 'some key to retreive' as unknown as U | undefined;
+  return { getValue };
+};
+
+const getData = createModuleConfig([
+  {
+    key: 'Field1',
+    schema: z.string(),
+  },
+  {
+    key: 'Field2',
+    schema: z.number(),
+  },
+]);
+
+const value1 = getData.getValue('Field1');
+//      ^?
+const value2 = getData.getValue('Field2');
+//      ^?
+console.log({ value1, value2 });
