a6f9ad7df89d3cba2d711ec7435889c297bae001
Jérôme Meichelbeck <jerome.meichelbeck@focus.lu>
2023-09-19 14:06:55 +0200

    Add first tests (failing)

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,36 @@
-import { describe, expect, it } from 'vitest';
+import { beforeAll, describe, expect, it } from 'vitest';
+import { createModuleConfig } from '../src/index';
+import { z } from 'zod';
 
-describe('test', () => {
-  it('should be ok', () => {
-    expect(1).toBe(1);
+let moduleConfig: any;
+
+beforeAll(() => {
+  moduleConfig = createModuleConfig([
+    {
+      key: 'name',
+      type: 'text',
+      schema: z.string(),
+    },
+    {
+      key: 'age',
+      type: 'number',
+      schema: z.number(),
+    },
+  ]);
+});
+
+describe('createModuleConfig', () => {
+  it('should set a value correctly', () => {
+    moduleConfig.set('name', 'John');
+    expect(moduleConfig.get('name')).toBe('John');
+  });
+
+  it('should set a value correctly', () => {
+    moduleConfig.set('age', 42);
+    expect(moduleConfig.get('age')).toBe(42);
+  });
+
+  it('should throw an error when setting a value with the wrong type', () => {
+    expect(() => moduleConfig.set('age', '42')).toThrowError();
   });
 });
